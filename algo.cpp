e03bc619b60cbbc3c630b0f57bf7c0772f05c9a9
Veer <veerdhawalkatakdaunde5032@gmail.com>
2026-04-24 01:36:20 +0530

    first commit

---
+++ algo.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define vll vector<long long>
+#define vi vector<int>
+using namespace std;
+void solve()
+{
+    int n;
+    cin >> n;
+}
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
